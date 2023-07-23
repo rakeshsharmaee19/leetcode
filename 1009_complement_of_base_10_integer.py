af3dadfe0d705f75972ddcd75f2fbd4b502f3545
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-07-23 20:02:42 +0530

    Create 1009_complement_of_base_10_integer.py

---
+++ 1009_complement_of_base_10_integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        a  = str(bin(n))[2:]
+        for i in range(len(a)):
+            if a[i] == '0':
+                a = a[:i]+'1'+a[i+1:]
+            else:
+                a = a[:i]+'0'+a[i+1:]
+        return int(a,2)
