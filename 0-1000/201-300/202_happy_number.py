be04634fb83885ba728fce3828f3560267b2ef29
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-08-07 14:56:03 +0530

    Create 202_happy_number.py

---
+++ 0-1000/201-300/202_happy_number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        n=str(n)
+        ch=False
+        if n=='1':
+            ch=True
+        else:
+            while n not in ('2', '3', '4', '5', '6', '8','9'):
+                print(n,1)
+                s=0
+                for i in n:
+                    s+=int(i)**2
+                if s==1:
+                    ch=True
+                    break
+                else:
+                    n=str(s)
+        return ch
