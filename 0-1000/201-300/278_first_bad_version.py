1f4f678c85a8abe34e8f4c068a3f1dad24bd36ae
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-08-08 17:21:24 +0530

    Create 278_first_bad_version.py

---
+++ 0-1000/201-300/278_first_bad_version.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l=0
+        r=n-1
+        mi=n
+        while l<=r:
+            mid=(l+r)>>1
+            if isBadVersion(mid)==True:
+                r=mid-1
+                mi=min(mi, mid)
+            else:
+                l=mid+1         
+        return mi
