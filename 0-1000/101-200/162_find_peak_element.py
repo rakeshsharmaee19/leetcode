8d50c46801429cdf11782aaa3eb038f7ba61f539
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-08-10 14:54:57 +0530

    Create 162_find_peak_element.py

---
+++ 0-1000/101-200/162_find_peak_element.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left=0
+        right=len(nums)-1
+        if len(nums)==1:
+            return 0
+        while left<=right:
+            mid=(left+right)>>1
+            if (mid==0 or nums[mid]>=nums[mid-1] ) and (mid==len(nums)-1 or nums[mid]>=nums[mid+1]) :
+                return mid
+            elif nums[mid]<=nums[mid+1]:
+                left=mid+1
+            else:
+                right=mid-1
+        return -1
