3ffb53971dfbe5a6691d659937172b3464124119
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-08-18 15:04:57 +0530

    Create 1089_duplictae_zeros.py

---
+++ 1001-2000/1001-1100/1089_duplictae_zeros.py
@@ -0,0 +1,17 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        n=len(arr)
+        res=[0]*n
+        i=j=0
+        while i<n:
+            if arr[j]==0:
+                i+=2           
+            else:
+                res[i]=arr[j]
+                i+=1
+            j+=1
+        arr.clear()
+        arr.extend(res)
