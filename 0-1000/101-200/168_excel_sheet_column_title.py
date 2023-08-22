13902a60961c1f1781ac72ede14ac3b520f37606
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-08-22 16:14:47 +0530

    Create 168_excel_sheet_column_title.py

---
+++ 0-1000/101-200/168_excel_sheet_column_title.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        a=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        s=''
+        while columnNumber>0:
+            s=a[((columnNumber-1)%26)]+s
+            columnNumber=(columnNumber-1)//26
+        return s
