755051c680794d03830972d4a887c5279222d9b4
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-09-22 15:58:23 +0530

    Delete 27_remove_element.py

--- 27_remove_element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        while val in set(nums):
-            idx = nums.index(val)
-            nums.pop(idx)
-            nums.append('_')
-            count+=1
-        return len(nums)-count
